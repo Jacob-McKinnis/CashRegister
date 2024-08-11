414a0ac9a0fe9ae9690ec047a3a16842406fbd5f
Jacob-McKinnis <jacobpmckinnis@gmail.com>
2024-08-10 22:06:39 -0400

    feat: parse input file

--- getChange.py
+++ getChange.py
@@ -2,6 +2,7 @@
 # Last Edited: 08/10/2024
 
 import argparse
+import decimal
 import os
 import sys
 
@@ -9,6 +10,11 @@ import sys
 # A helper function to output debugging information 
 def debugPrint(info):
     if debugMode: print(info)
+    
+def errorQuit(error: str, file = None):
+    if file is not None:
+        file.close() 
+    sys.exit(error)
 
 # 1. Setup
 ## 1.1 Set up permitted arguments
@@ -34,4 +40,32 @@ debugPrint(f"File: {args.file}")
 debugPrint("")
 
 if not os.path.isfile(args.file):
-    sys.exit(f"Invalid file: {args.file}")
+    errorQuit(f"Invalid file: {args.file}")
+    
+# 2. Process input file
+f = open(args.file, "r")
+output = []
+for x in f:
+    line = x.rstrip('\r\n')
+    debugPrint(f"Line: {line}")
+    
+    if "," not in line:
+        errorQuit(f"Invalid file formatting: '{line}' does not have a comma", f)
+        
+    parts = line.split(",")
+    debugPrint(f"Parts: {parts}")
+    
+    if len(parts) != 2:
+        errorQuit(f"Invalid file formatting: '{line}' does not have two values", f)
+        
+    try:
+        owed = decimal.Decimal(parts[0])
+        paid = decimal.Decimal(parts[1])
+    except:
+        errorQuit(f"Invalid values: unable to convert values in '{line}' to currency", f)
+        
+    debugPrint(f"Owed: {owed}")
+    debugPrint(f"Paid: {paid}")
+    
+    debugPrint("")
+f.close() 
